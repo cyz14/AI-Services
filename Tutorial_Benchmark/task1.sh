b17e48b39a225f60f886239cb3545ee6568a4dd1
Chen Yazheng <cyz14@users.noreply.github.com>
2019-08-13 12:25:53 +0800

    adjust to install package on Taiwania 2

--- Tutorial_Benchmark/task1.sh
+++ Tutorial_Benchmark/task1.sh
@@ -3,6 +3,7 @@ cd benchmarks
 git checkout num_steps_issue_1.10
 cd ..
 
-pip install --user terminaltables 
+# pip install --user terminaltables 
+pip --no-cache-dir install terminaltables 
 
 python py/task1.py
