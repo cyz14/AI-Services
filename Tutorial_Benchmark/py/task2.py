b676577f63502e5b32b7f9bb1daaef048b77c4fe
Chen Yazheng <cyz14@users.noreply.github.com>
2019-08-13 12:26:40 +0800

    adjust to run on Taiwania 2

--- Tutorial_Benchmark/py/task2.py
+++ Tutorial_Benchmark/py/task2.py
@@ -14,7 +14,7 @@ p = os.getcwd()
 for i in num_gpu:
     print('G{}B32'.format(i))
     print(i)
-    a = subprocess.check_output("/usr/local/mpi/bin/mpirun -np {} -H localhost:{} python {}/horovod/examples/tensorflow_synthetic_benchmark.py".format(i,i,p),shell=True)
+    a = subprocess.check_output("mpirun -np {} -H localhost:{} python {}/horovod/examples/tensorflow_synthetic_benchmark.py".format(i,i,p),shell=True)
     b = a.decode("utf-8")
     c = b.split('\n')[-2]
     task1['G{}B32'.format(i)]=c 
